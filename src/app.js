2b38ffb3846062bc5e7592f861175e3b05a1c055
MatiasB <matiasburdisso17@gmail.com>
2023-03-16 09:26:09 -0300

    planillas

--- src/app.js
+++ src/app.js
@@ -1,64 +1,31 @@
+import { engine } from "express-handlebars";
 import express from "express";
-import path from "path";
-import ProductManager from "./ProductManager.js";
+import viewsRoute from "../routes/views.router.js";
+import utils from "./utils.js";
+
 
 const app = express();
-const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use(express.static("public"));
 
-const productManager = new ProductManager(
-  path.resolve(process.cwd(), "public", "products.json")
-);
+app.use("/views", viewsRoute);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.get("/formulario", (req, res) => {
+  let testUser = {
+    title: "Este es un formulario",
+    message: "Hola gracias por registrarte!",
+    name: "Jose",
+  };
+  res.render("form", testUser);
 });
 
-app.get("/productos", async (req, res) => {
-  try {
-    const products = await productManager.getProducts();
-    const limit = req.query.limit;
-    let limitedProducts;
-    if (limit) {
-      limitedProducts = products.slice(0, limit);
-    }
-    res.send(limitedProducts || products);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
 
-// create a route for the app
-app.post("/productos", async (req, res) => {
-  try {
-    const products = await productManager.getProducts();
-    const newProduct = req.body;
-    await productManager.addProduct(products, newProduct);
-    res.send(newProduct);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
-
-app.get("/productos/:id", async (req, res) => {
-  try {
-    const products = await productManager.getProducts();
-    const id = req.params.id;
-    const product = products.find((product) => product.id === id);
-    if (!product) {
-      res.status(404).send("Producto no encontrado");
-    }
-    res.send(product);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
-
-//start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Iniciado en http://localhost:${port}`);
+  console.log(`Server listening on port ${port}`);
 });
