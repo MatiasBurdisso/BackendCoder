68b538e4e5eeaa02ae54b789264f0f9518f621ca
MatiasB <matiasburdisso17@gmail.com>
2023-05-30 18:44:11 -0300

    tickets

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import ManagerMongoDb from "../dao/ManagerMongoDB.js";
+import {v4 as uuidv4} from 'uuid';
+import { ticketsModel } from "../models/ticket.model.js";
 
 const router = Router();
 const cartManager = new ManagerMongoDb.CartManager();
@@ -60,5 +62,44 @@ router.delete("/:cid", async (req, res) => {
     req.status(500).send(err.message);
   }
 });
+
+router.post("/:cid/purchase",async(req,res)=>{
+  try {
+      const cartId = req.params.cid;
+      const cart = await CartModel.findById(cartId);
+      if(cart){
+          if(!cart.products.length){
+              return res.send("es necesario que agrege productos antes de realizar la compra")
+          }
+          const ticketProducts = [];
+          const rejectedProducts = [];
+          for(let i=0; i<cart.products.length;i++){
+              const cartProduct = cart.products[i];
+              const productDB = await ProductModel.findById(cartProduct.id);
+              //comparar la cantidad de ese producto en el carrito con el stock del producto
+              if(cartProduct.quantity<=productDB.stock){
+                  ticketProducts.push(cartProduct);
+              } else {
+                  rejectedProducts.push(cartProduct);
+              }
+          }
+          console.log("ticketProducts",ticketProducts)
+          console.log("rejectedProducts",rejectedProducts)
+          const newTicket = {
+              code:uuidv4(),
+              purchase_datetime: new Date().toLocaleString(),
+              amount:500,
+              purchaser:req.user.email
+          }
+          const ticketCreated = await ticketsModel.create(newTicket);
+          res.send(ticketCreated)
+      } else {
+          res.send("el carrito no existe")
+      }
+  } catch (error) {
+      res.send(error.message)
+  }
+});
+
 
 export default router;
